ec6014cae46142bc1d14dadd2314a6b8b1d51eb5
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-07-07 19:15:14 +0300

    add case "ADD-USER"

--- src/bll/usersReducer.ts
+++ src/bll/usersReducer.ts
@@ -2,7 +2,7 @@ import {userAPI, UserType} from "../api/api";
 import {Dispatch} from "redux";
 
 const initialState = {
-  users: [] as Array<UserType>
+  users: [] as Array<UserType>,
 }
 
 
@@ -12,17 +12,18 @@ export const usersReducer = (state: InitialStateType = initialState, action: Use
     return {
       ...state,
       users: [...action.users]
-
     }
+    case "ADD-USER":
+      return {
+        ...state,
+        users: state.users.map(u => u.id === action.id ? {...u, followed: !u.followed } : u)
+      }
   }
   return state
 }
-//Type
-export type InitialStateType = typeof initialState
-export type UsersReducerAT = getUserAT
-type getUserAT = ReturnType<typeof getUsersAC>
 //Actions
 export const getUsersAC = (users: Array<UserType>) => ({type: 'SET-USER', users} as const)
+export const addUserAC = (id: number) => ({type: 'ADD-USER', id} as const)
 //Thunk
 export const getUserTC = (currentPageNumber: number = 10, pageSize: number = 10) => {
   return (dispatch: Dispatch) => {
@@ -30,3 +31,8 @@ export const getUserTC = (currentPageNumber: number = 10, pageSize: number = 10)
       .then(res => dispatch(getUsersAC(res.data.items)))
   }
 }
+//Type
+export type InitialStateType = typeof initialState
+export type UsersReducerAT = getUserAT | addUserAT
+type getUserAT = ReturnType<typeof getUsersAC>
+type addUserAT = ReturnType<typeof addUserAC>
