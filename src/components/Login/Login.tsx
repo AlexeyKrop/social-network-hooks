4a0545b5cce204d63685e24d53438a883844430a
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-07-18 14:53:08 +0300

    add input

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,35 +1,19 @@
 import React from 'react';
-import {Controller, SubmitHandler, useForm} from "react-hook-form";
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import LoginIcon from '@mui/icons-material/Login';
+import {SubmitHandler, useForm} from "react-hook-form";
+
 type Inputs = {
   login: string,
   password: string,
 };
 export const Login = () => {
 
-  const { register, control, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+  const {register, control, handleSubmit, watch, formState: {errors}} = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <Controller
-        render={({ field }) => <TextField {...field} label="login" variant="outlined" /> }
-        name="login"
-        control={control}
-        defaultValue=""
-      />
-      <Controller
-        render={({ field }) => <TextField {...field} type='password' label="password" variant="outlined" />}
-        name="password"
-        control={control}
-        defaultValue=""
-      />
-      {errors.password && <span>This field is required</span>}
-      {errors.login && <span>This field is required</span>}
-      <Button variant="contained" endIcon={<LoginIcon />}>
-        Enter
-      </Button>
+      <input {...register("login")} />
+      <input type='password' {...register("password")} />
+      <input type="submit"/>
     </form>
   );
 };
