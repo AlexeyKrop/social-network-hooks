dcc12bb4a3f449922e0d7ab5792902ee53ba1005
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-07-18 11:47:21 +0300

    add start router

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,12 @@ export const App = React.memo(() => {
         <Sidebar/>
         <main>
           <Routes>
+            <Route path="/" element={<Profile />}/>
             <Route path='profile/' element={<Profile/>}/>
             <Route path='/profile/:id' element={<Profile/>}/>
             <Route path="/friends/" element={<Friends/>}/>
             <Route path="/dialogs/" element={<Dialogs/>}/>
+
           </Routes>
         </main>
       </div>
