d25493a0a017d1098c097f593d44e5d709e06e5f
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-07-18 12:10:52 +0300

    add active style

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -23,7 +23,6 @@ const listItems = [
     nav: "/dialogs"
   }
 ];
-
 export const Sidebar = () => {
   return (
     <>
@@ -31,7 +30,9 @@ export const Sidebar = () => {
       <Box className={s.wrapper} component="div">
         <List>
           {listItems.map((listItem, index) => (
-            <NavLink className={s.link}  key={index} to={listItem.nav}><ListItem button >
+            <NavLink  className={({ isActive }) =>
+              isActive ? `${s.active}` : `${s.link}`
+            } key={index} to={listItem.nav}><ListItem button >
               <ListItemIcon>
                 {listItem.listIcon}
               </ListItemIcon>
